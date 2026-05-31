7e86ce2427f1928bc363b042ffaf6e7b2f647854
Plotchuvision44 <v00314083@gmail.com>
2026-05-30 22:53:15 -0400

    feat: add src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "roblox/roblox_script.h"
+
+int main() {
+    std::cout << "Brookhaven RP Roblox Utility\n";
+    RobloxScript script;
+    script.execute("print('Hello from Brookhaven RP!')");
+    return 0;
+}
